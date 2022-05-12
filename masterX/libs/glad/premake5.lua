53bd884f0c2843253796026d90a300503e2a2729
YsfHam <ysf.hamdouni@gmail.com>
2022-05-12 12:40:22 +0200

    Adding imgui submodule

---
+++ masterX/libs/glad/premake5.lua
@@ -0,0 +1,14 @@
+project "glad"
+	kind "StaticLib"
+	language "C"
+
+	location "%{wks.location}/build"
+    targetdir "%{wks.location}/bin/bin/%{cfg.name}_%{cfg.architecture}"
+    objdir "%{wks.location}/bin/obj/%{cfg.name}_%{cfg.architecture}"
+
+	files
+	{
+		"src/glad.c"
+	}
+
+	includedirs "include"
